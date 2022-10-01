ef23c33a8bb1520ceaf29038fcf3680e9effa4db
GuiSparda <ribeiroguilherme72@gmail.com>
2022-10-01 19:58:55 -0300

    Jogo do rato v0.1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D body;
+    public float speed;
+    public Keyboard keyboard;
+    public GameObject mouseSprite;
+    Vector2 move;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+        body = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSprite();
+        Debug.Log(move);
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = move*Mathf.Round(speed);
+    }
+
+    void UpdateSprite() {
+        if (move==Vector2.up)
+        {
+            mouseSprite.transform.rotation = Quaternion.Euler(new Vector3(0,0,180));
+        }
+
+        if (move == Vector2.down)
+        {
+            mouseSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+
+        if (move == Vector2.left)
+        {
+            mouseSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -90));
+        }
+
+        if (move == Vector2.right)
+        {
+            mouseSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+
+    }
+
+    public void Iniciar() {
+        Debug.Log("Iniciou");
+        
+        move = Vector2.right;
+    }
+
+    public void MoverEsquerda() {
+        Debug.Log("Esquerda");
+        move = Vector2.up;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+}
